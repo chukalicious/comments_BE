f53941ba425d55b527f82d309362a80efcc88919
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-23 21:17:56 -0500

    installed hemlet, cors and jwt

--- api/server.js
+++ api/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const UsersRouter = require("./users/users-router");
 const CommentsRouter = require("./comments/comments-router");
@@ -8,6 +10,8 @@ const AuthRouter = require("./auth/auth-router");
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
 server.use("/users", UsersRouter);
 server.use("/users/signup", AuthRouter);
