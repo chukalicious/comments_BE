a2bac9cfe08d994efd2c7e5c243a600bf8ad2fbc
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-12-30 02:48:07 -0500

    fixed error message

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -21,7 +21,7 @@ router.get("/:id", (req, res) => {
       if (user) {
         res.status(200).json(user);
       } else {
-        res.status(404).json({ message: "Adopter not found" });
+        res.status(404).json({ message: "User not found" });
       }
     })
     .catch((error) => {
